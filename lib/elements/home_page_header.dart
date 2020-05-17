be216a7e07b9478736589670ef9d7f276f04a0b7
SBaro <santaigo.baro@fing.edu.uy>
2020-05-17 14:02:08 -0300

    fixed audio controls UI bug

--- lib/elements/home_page_header.dart
+++ lib/elements/home_page_header.dart
@@ -40,14 +40,16 @@ class HomePageHeader extends StatelessWidget {
           children: <Widget>[
             //* START SEPARATOR
             // TO MAKE THE FIRST ELEMENT CENTERED
-
             SizedBox(
               width: MediaQuery.of(context).size.width * 0.1 - 7,
             ),
 
             //* TILE [0]
             Padding(
-              padding: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10.0,
+                vertical: 15,
+              ),
               child: Container(
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(10.0),
@@ -80,73 +82,87 @@ class HomePageHeader extends StatelessWidget {
 
             //* TILE [1]
             Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Container(
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10.0),
-                  child: Stack(
-                    children: <Widget>[
-                      Image(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10.0,
+                vertical: 15,
+              ),
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10.0),
+                      child: Image(
                         image: NetworkImage(
                           'http://podcasts.joerogan.net/wp-content/uploads/2020/05/JRE1472.jpg',
                         ),
                         fit: BoxFit.fill,
                       ),
-                      Container(
-                        color: Colors.black38,
-                      ),
-                      Center(
-                        child: Row(
-                          children: <Widget>[
-                            IconButton(
-                              iconSize: 60,
-                              color: Colors.white,
-                              icon: Icon(Icons.replay_10),
-                              tooltip: 'Increase volume by 10',
-                              onPressed: () {},
-                            ),
-                            IconButton(
-                              iconSize: 90,
-                              color: Colors.white,
-                              icon: Icon(Icons.pause_circle_outline),
-                              tooltip: 'Increase volume by 10',
-                              onPressed: () {},
-                            ),
-                            IconButton(
-                              iconSize: 60,
-                              color: Colors.white,
-                              icon: Icon(Icons.forward_10),
-                              tooltip: 'Increase volume by 10',
-                              onPressed: () {},
-                            ),
-                          ],
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisSize: MainAxisSize.max,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                decoration: new BoxDecoration(
-                    color: Colors.black12,
-                    borderRadius: new BorderRadius.all(Radius.circular(10.0)),
-                    boxShadow: [
-                      BoxShadow(
+                    ),
+                    decoration: new BoxDecoration(
                         color: Colors.black12,
-                        offset: const Offset(0.0, 8.0),
-                        blurRadius: 5.0,
-                        spreadRadius: 2.0,
-                      ),
-                    ]),
-                height: MediaQuery.of(context).size.width * 0.8 * 9 / 14,
-                width: MediaQuery.of(context).size.width * 0.8,
+                        borderRadius:
+                            new BorderRadius.all(Radius.circular(10.0)),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            offset: const Offset(0.0, 8.0),
+                            blurRadius: 5.0,
+                            spreadRadius: 2.0,
+                          ),
+                        ]),
+                    height: MediaQuery.of(context).size.width * 0.8 * 9 / 14,
+                    width: MediaQuery.of(context).size.width * 0.8,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.black38,
+                      borderRadius: new BorderRadius.all(Radius.circular(10.0)),
+                    ),
+                    height: MediaQuery.of(context).size.width * 0.8 * 9 / 14,
+                    width: MediaQuery.of(context).size.width * 0.8,
+                  ),
+                  Container(
+                    child: Row(
+                      children: <Widget>[
+                        IconButton(
+                          iconSize: 60,
+                          color: Colors.white,
+                          icon: Icon(Icons.replay_10),
+                          tooltip: 'Increase volume by 10',
+                          onPressed: () {},
+                        ),
+                        IconButton(
+                          iconSize: 90,
+                          color: Colors.white,
+                          icon: Icon(Icons.pause_circle_outline),
+                          tooltip: 'Increase volume by 10',
+                          onPressed: () {},
+                        ),
+                        IconButton(
+                          iconSize: 60,
+                          color: Colors.white,
+                          icon: Icon(Icons.forward_10),
+                          tooltip: 'Increase volume by 10',
+                          onPressed: () {},
+                        ),
+                      ],
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                    ),
+                    height: MediaQuery.of(context).size.width * 0.8 * 9 / 14,
+                    width: MediaQuery.of(context).size.width * 0.8,
+                  ),
+                ],
               ),
             ),
 
             //* TILE [2]
             Padding(
-              padding: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10.0,
+                vertical: 15,
+              ),
               child: Container(
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(10.0),
@@ -174,7 +190,10 @@ class HomePageHeader extends StatelessWidget {
 
             //* TILE [3]
             Padding(
-              padding: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10.0,
+                vertical: 15,
+              ),
               child: Container(
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(10.0),
@@ -205,7 +224,10 @@ class HomePageHeader extends StatelessWidget {
 
             //* TILE [4]
             Padding(
-              padding: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10.0,
+                vertical: 15,
+              ),
               child: Container(
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(10.0),
